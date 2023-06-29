7602c65452e8ab0e61085ff7f1d5bf53cd4e6163
Tyler Wolf <tylerwolf@posteo.net>
2023-06-29 16:05:46 -0400

    update emacs init.el

--- Users/tyler/.emacs.d/init.el
+++ Users/tyler/.emacs.d/init.el
@@ -131,7 +131,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(centaur-tabs meow evil-collection undo-fu autothemer evil dashboard)))
+   '(elpher centaur-tabs meow evil-collection undo-fu autothemer evil dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
