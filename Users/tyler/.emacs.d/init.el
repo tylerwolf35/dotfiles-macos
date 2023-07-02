a73709a11b2c72b75ecc887736e187d53f0e2260
Tyler Wolf <tylerwolf@posteo.net>
2023-07-01 21:22:47 -0400

    fix native comp issues

--- Users/tyler/.emacs.d/init.el
+++ Users/tyler/.emacs.d/init.el
@@ -1,5 +1,14 @@
 ;; here is some housekeeping :)
 
+;; HACK Work around native compilation on macOS failing with 'ld: library not
+;; found for -lemutls_w'.
+;; https://github.com/d12frosted/homebrew-emacs-plus/issues/554
+(setenv "LIBRARY_PATH"
+	(string-join
+	 '("/opt/homebrew/opt/gcc/lib/gcc/13"
+	   "/opt/homebrew/opt/libgccjit/lib/gcc/13"
+	   "/opt/homebrew/opt/gcc/lib/gcc/13/gcc/aarch64-apple-darwin22/13")
+	 ":"))
 
 ;; line numbers
 (global-display-line-numbers-mode)
@@ -131,7 +140,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elpher centaur-tabs meow evil-collection undo-fu autothemer evil dashboard)))
+   '(elpher centaur-tabs meow evil-collection undo-fu autothemer evil
+	    dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
