38a2e73563e65112e9c41ea46856a0172829c2ea
Tyler Wolf <tylerwolf@posteo.net>
2023-07-02 13:05:25 -0400

    update emacs config

--- Users/tyler/.emacs.d/init.el
+++ Users/tyler/.emacs.d/init.el
@@ -139,12 +139,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(elpher centaur-tabs meow evil-collection undo-fu autothemer evil
-	    dashboard)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages '(elpher centaur-tabs meow autothemer dashboard)))
