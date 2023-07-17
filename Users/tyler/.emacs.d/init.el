fcf22c0ff7780050e208783345ad9a9c535c9e28
Tyler Wolf <tylerwolf@posteo.net>
2023-07-17 16:22:28 -0400

    update emacs

--- Users/tyler/.emacs.d/init.el
+++ Users/tyler/.emacs.d/init.el
@@ -140,14 +140,18 @@
   (ement-connect :uri-prefix "http://localhost:8009"
 		 :user-id "@tylerwolf:matrix.org"))
 
+;; shell
+(when (daemonp)
+  (exec-path-from-shell-initialize))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(with-editor magit vterm ement elpher centaur-tabs meow autothemer
-		 dashboard)))
+   '(exec-path-from-shell with-editor magit vterm ement elpher
+			  centaur-tabs meow autothemer dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
