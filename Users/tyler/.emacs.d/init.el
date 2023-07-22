f1af851b5e81e5538ecfaef04077d67658ab2cd4
Tyler Wolf <tylerwolf@posteo.net>
2023-07-22 11:44:20 -0400

    update

--- Users/tyler/.emacs.d/init.el
+++ Users/tyler/.emacs.d/init.el
@@ -150,8 +150,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(exec-path-from-shell with-editor magit vterm ement elpher
-			  centaur-tabs meow autothemer dashboard)))
+   '(auctex exec-path-from-shell with-editor magit vterm ement elpher
+	    centaur-tabs meow autothemer dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
